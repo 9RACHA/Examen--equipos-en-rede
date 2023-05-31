d7189d7de9746d530e3bafa57bd2bfb2c9f29116
9RACHA <66274956+9RACHA@users.noreply.github.com>
2023-05-31 16:39:21 +0200

    Tutorial desde 0
    Ejercicio 2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,21 @@
 using Unity.Netcode;
 using UnityEngine;
 
-    public class GameManager : NetworkBehaviour
+namespace HelloWorld
+{
+    public class GameManager : MonoBehaviour
     {
         void OnGUI()
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            
             if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
             {
                 StartButtons();
             }
-            else 
+            else
             {
+                StatusLabels();
+
                 SubmitNewPosition();
             }
 
@@ -23,39 +26,35 @@ using UnityEngine;
         {
             if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
             if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-        }
-
-        static void SubmitNewPosition(){
-            if (GUILayout.Button("Equipo Azul"))
-            {
-                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                var player = playerObject.GetComponent<Player>();
-                player.Mueve(1);
-            }
-
-            if (GUILayout.Button("Sin equipo"))
-            {
-                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                var player = playerObject.GetComponent<Player>();
-                player.Mueve(0);
-            }
-
-            if (GUILayout.Button("Equipo Rojo"))
-            {
-                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                var player = playerObject.GetComponent<Player>();
-                player.Mueve(2);
-            }
+            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
         }
 
         static void StatusLabels()
         {
-            
             var mode = NetworkManager.Singleton.IsHost ?
-                "Host" : NetworkManager.Singleton.IsClient ? "Client" : "Client";
+                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
 
             GUILayout.Label("Transport: " +
                 NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
             GUILayout.Label("Mode: " + mode);
-        }    
+        }
+
+        static void SubmitNewPosition()
+        {
+            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
+            {
+                if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient )
+                {
+                    foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().Move();
+                }
+                else
+                {
+                    var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+                    var player = playerObject.GetComponent<Player>();
+                    player.Move();
+                }
+            }
+        }
     }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,158 +1,48 @@
-using UnityEngine;
-using UnityEngine.Networking;
 using Unity.Netcode;
-using Unity.Netcode.Components;
-using System.Collections.Generic;
+using UnityEngine;
 
-public class Player : NetworkBehaviour {
+namespace HelloWorld
+{
+    public class Player : NetworkBehaviour
+    {
+        public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
 
-    public float velocidadMovimiento = 4f;
-    public Rigidbody rb;
-    public Renderer r;
-    public int equipoAnterior;
-
-    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
-
-    public NetworkVariable<Color> color;
-
-    public static List<GameObject> equipoAzul;
-    public static List<GameObject> equipoRojo;
-    
-   // public static NetworkList<GameObject> equipoRojo;
-
-    //public NetworkList(IEnumerable<T> values = null, NetworkVariableReadPermission readPerm = NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission writePerm = NetworkVariableWritePermission.Server);
-   
-    public override void OnNetworkSpawn(){
-        if (IsOwner)
+        public override void OnNetworkSpawn()
         {
-            RequestInitialPositionServerRpc();
-        }
-    }
-
-    [ServerRpc]
-    void RequestInitialPositionServerRpc(){
-        Position.Value = GetRandomPositionOnPlane();
-        UpdatePositionClientRpc(Position.Value);
-    }
-
-    [ServerRpc]
-    void RequestPositionChangeServerRpc(Vector3 direction){
-        Position.Value += direction;
-        UpdatePositionClientRpc(Position.Value);
-    }
-
-    [ClientRpc]
-    void UpdatePositionClientRpc(Vector3 newPosition){
-        if (!IsOwner)
-        {
-            Position.Value = newPosition;
-        }
-
-    [ServerRpc]
-    void SubmitPositionRequestServerRpc(int equipo = 0, ServerRpcParams rpcParams = default){
-        Position.Value = GetRandomPositionOnPlane();
-        EquipoSeleccionado(equipo);
-
-        if (equipo == 1)
-        {
-            equipoAzul.Add(gameObject);
-        }
-        else if( equipo == 2) {
-            equipoRojo.Add(gameObject);
-        }
-
-        Debug.Log("Integrantes azules:" + equipoAzul.Count);
-        Debug.Log("Integrantes rojos:" + equipoRojo.Count);
-    }
-
-     void Mueve(int equipo = 0) {
-        if (equipoAzul.Count >= 2 && equipo == 1) {
-            equipo = equipoAnterior;
-            Debug.Log("Equipo azul lleno");
-        }
-            else if (equipoRojo.Count >= 2 && equipo == 2){
-                equipo = equipoAnterior;
-                Debug.Log("Equipo rojo lleno");
-            } else {
-                equipoAnterior = equipo;
-                SubmitPositionRequestServerRpc(equipo);
-            }
-        } 
-
-
-    void EquipoSeleccionado(int equipo = 0){
-        if (equipo == 0){
-            color.Value = Color.white;
-    } else if (equipo == 1){
-        color.Value = Color.blue;
-    } else if (equipo == 2){
-        color.Value = Color.red;
-    }
-    Debug.Log("El valor del equipo es " + equipo);
-    }
-
-   static Vector3 GetRandomPositionOnPlane(int equipo = 0)
-        {
-            if (equipo == 0)
+            if (IsOwner)
             {
-                return new Vector3(Random.Range(-1f, 1f), 1f, Random.Range(-3f, 3f));
+                Move();
             }
-            else if (equipo == 1)
+        }
+
+        public void Move()
+        {
+            if (NetworkManager.Singleton.IsServer)
             {
-                return new Vector3(Random.Range(-3f, -1f), 1f, Random.Range(-3f, 3f));
+                var randomPosition = GetRandomPositionOnPlane();
+                transform.position = randomPosition;
+                Position.Value = randomPosition;
             }
             else
             {
-                return new Vector3(Random.Range(1f, 3f), 1f, Random.Range(-3f, 3f));
+                SubmitPositionRequestServerRpc();
             }
         }
 
-    void OnPositionChanged(Vector3 antiguaPos, Vector3 nuevaPos){
-        transform.position = Position.Value;
-    }
+        [ServerRpc]
+        void SubmitPositionRequestServerRpc(ServerRpcParams rpcParams = default)
+        {
+            Position.Value = GetRandomPositionOnPlane();
+        }
 
-    void OnColorChanged(Color antiguoColor, Color nuevoColor) {
-        r.material.color = color.Value;
-    }
+        static Vector3 GetRandomPositionOnPlane()
+        {
+            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+        }
 
-    
-     void Awake(){
-        r = GetComponent<Renderer>();
-        equipoAzul = new List<GameObject>();
-        equipoRojo = new List<GameObject>();
-    }
-
-     void Start() {
-        rb = GetComponent<Rigidbody>();
-        Position.OnValueChanged += OnPositionChanged;
-        color.OnValueChanged += OnColorChanged;
-    }
-
-     void Update() {
-
-        if (IsOwner)
-            {
-                Vector3 direction = Vector3.zero;
-
-                // Detecta las flechas para determinar la dirección del movimiento
-                if (Input.GetKeyDown(KeyCode.LeftArrow))
-                    direction = Vector3.left;
-                else if (Input.GetKeyDown(KeyCode.RightArrow))
-                    direction = Vector3.right;
-                else if (Input.GetKeyDown(KeyCode.DownArrow))
-                    direction = Vector3.back;
-                else if (Input.GetKeyDown(KeyCode.UpArrow))
-                    direction = Vector3.forward;
-
-                // Si se ha presionado alguna tecla de flecha, solicita el cambio de posición al servidor
-                if (direction != Vector3.zero)
-                    RequestPositionChangeServerRpc(direction);
-            }
-
-            // Actualiza la posición del objeto en el mundo del juego basándose en el valor actual de la variable de red Position, 
-            // asegurando que el movimiento se sincronice correctamente en todos los clientes
+        void Update()
+        {
             transform.position = Position.Value;
-
-    }
+        }
     }
 }
