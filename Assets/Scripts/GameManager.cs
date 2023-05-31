dccce807fb07722eebdf03dd34e6982eec01562d
9RACHA <66274956+9RACHA@users.noreply.github.com>
2023-05-31 17:30:49 +0200

    Ejercicio 2 : Añadido movimiento del jugador

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,12 @@
 using Unity.Netcode;
 using UnityEngine;
 
-namespace HelloWorld
-{
     public class GameManager : MonoBehaviour
     {
         void OnGUI()
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer) //Si no esta conectado
             {
                 StartButtons();
             }
@@ -43,18 +41,13 @@ namespace HelloWorld
         {
             if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Move" : "Request Position Change"))
             {
-                if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient )
-                {
-                    foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
-                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid).GetComponent<Player>().Move();
+                if (NetworkManager.Singleton.IsServer && !NetworkManager.Singleton.IsClient ){
                 }
                 else
                 {
                     var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
                     var player = playerObject.GetComponent<Player>();
-                    player.Move();
                 }
             }
         }
     }
-}
